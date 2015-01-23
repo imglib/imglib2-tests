70310eac45b72561e4fe03db51b67776e838e9f2
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-01-23 17:38:35 +0100

    Interactive test for the Periodic Line structuring element.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/test/java/net/imglib2/algorithm/morphology/PeriodicLineNeighborhoodTest.java
@@ -0,0 +1,39 @@
+package net.imglib2.algorithm.morphology;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+
+import java.io.File;
+
+import net.imglib2.algorithm.morphology.neighborhoods.PeriodicLineShape;
+import net.imglib2.algorithm.region.localneighborhood.Shape;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+public class PeriodicLineNeighborhoodTest {
+
+	public static <T extends RealType<T> & NativeType<T>> void main(final String[] args) {
+
+		ImageJ.main(args);
+		final File file = new File( "DrosophilaWing.tif" );
+		final ImagePlus imp = IJ.openImage(file.getAbsolutePath());
+		final Img<T> img = ImagePlusAdapter.wrap(imp);
+
+		final long start = System.currentTimeMillis();
+
+		final Shape shape = new PeriodicLineShape(2, new int[] { 20, -15 });
+		final Img< T > target = Erosion.erode( img, shape, 1 );
+
+		final long end = System.currentTimeMillis();
+
+		System.out.println( "Processing done in " + ( end - start ) + " ms." );
+
+		ImageJFunctions.show(img);
+		ImageJFunctions.show(target);
+
+	}
+}
