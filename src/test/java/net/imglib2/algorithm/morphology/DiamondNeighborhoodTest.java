bf30808c06c4a71c76250912dfef03ecf98d3e5b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-01-23 17:31:55 +0100

    Interactive tests for the Diamond structuring element.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/test/java/net/imglib2/algorithm/morphology/DiamondNeighborhoodTest.java
@@ -0,0 +1,161 @@
+package net.imglib2.algorithm.morphology;
+
+
+import ij.ImageJ;
+
+import java.util.List;
+
+import net.imglib2.algorithm.region.localneighborhood.Shape;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.array.ArrayRandomAccess;
+import net.imglib2.img.basictypeaccess.array.ByteArray;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+
+
+public class DiamondNeighborhoodTest
+{
+
+	public static void main( final String[] args )
+	{
+		main0( args ); // Look at strels
+		main1( args ); // Benchmark 3D case
+		main3( args ); // Benchmark 2D case
+		main7( args ); // Print the decomposition of a 3D strel
+	}
+
+	public static void main0( final String[] args )
+	{
+		ImageJ.main( args );
+		final ArrayImg< UnsignedByteType, ByteArray > img = ArrayImgs.unsignedBytes( 19, 19, 19 );
+		final ArrayRandomAccess< UnsignedByteType > ra = img.randomAccess();
+		ra.setPosition( new long[] { 9, 9, 9 } );
+		ra.get().set( 255 );
+
+		final int[] radiuses = new int[] { 2, 3, 5 };
+		for ( final int radius : radiuses )
+		{
+			final List< Shape > strelStd = StructuringElements.diamond( radius, img.numDimensions(), false );
+			final Img< UnsignedByteType > std = Dilation.dilate( img, strelStd, 1 );
+			final List< Shape > strelOpt = StructuringElements.diamond( radius, img.numDimensions(), true );
+			final Img< UnsignedByteType > opt = Dilation.dilate( img, strelOpt, 1 );
+
+			ImageJFunctions.show( std, "Std " + radius );
+			ImageJFunctions.show( opt, "Opt " + radius );
+		}
+	}
+
+	/**
+	 * Performance comparison between optimized & standard strel, 3D case.
+	 */
+	public static void main1( final String[] args )
+	{
+		final ArrayImg< UnsignedByteType, ByteArray > img = ArrayImgs.unsignedBytes( 39, 39, 39 );
+		final ArrayRandomAccess< UnsignedByteType > ra = img.randomAccess();
+		ra.setPosition( new long[] { 19, 19, 19 } );
+		ra.get().set( 255 );
+
+		System.out.println( "Full strel" );
+		System.out.println( "radius\ttime(ms)" );
+		// Warm up
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), false ), 1 );
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), false ), 1 );
+		for ( int i = 1; i < 19; i++ )
+		{
+			final long start = System.currentTimeMillis();
+			final List< Shape > strels = StructuringElements.diamond( i, img.numDimensions(), false );
+			Dilation.dilate( img, strels, 1 );
+			final long end = System.currentTimeMillis();
+			System.out.println( "" + i + '\t' + ( end - start ) );
+		}
+
+		System.out.println( "Decomposed strel" );
+		System.out.println( "radius\ttime(ms)" );
+		// Warm up
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), true ), 1 );
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), true ), 1 );
+		for ( int i = 1; i < 19; i++ )
+		{
+			final long start = System.currentTimeMillis();
+			final List< Shape > strels = StructuringElements.diamond( i, img.numDimensions(), true );
+			Dilation.dilate( img, strels, 1 );
+			final long end = System.currentTimeMillis();
+			System.out.println( "" + i + '\t' + ( end - start ) );
+		}
+	}
+
+	/**
+	 * Tune optimized 3D cases
+	 *
+	 * @param args
+	 */
+	public static void main7( final String[] args )
+	{
+		ImageJ.main( args );
+
+		final ArrayImg< UnsignedByteType, ByteArray > img = ArrayImgs.unsignedBytes( 19, 19, 19 );
+		final ArrayRandomAccess< UnsignedByteType > ra = img.randomAccess();
+		ra.setPosition( new long[] { 9, 9, 9 } );
+		ra.get().set( 255 );
+
+		final long start = System.currentTimeMillis();
+		final List< Shape > strels = StructuringElements.diamond( 6, img.numDimensions(), true );
+
+		Img< UnsignedByteType > dilated = img;
+		for ( final Shape strel : strels )
+		{
+			final String str = MorphologyUtils.printNeighborhood( strel, img.numDimensions() );
+			System.out.println( str );
+			dilated = Dilation.dilate( dilated, strel, 1 );
+			ImageJFunctions.show( dilated, "Decomposed strel: " + strel );
+		}
+
+		final long end = System.currentTimeMillis();
+		System.out.println( "Optimized processing time: " + ( end - start ) + " ms." );
+
+
+	}
+
+	/**
+	 * Performance comparison between optimized & standard strel, 2D case.
+	 */
+	public static void main3( final String[] args )
+	{
+		ImageJ.main( args );
+
+		final ArrayImg< UnsignedByteType, ByteArray > img = ArrayImgs.unsignedBytes( 100, 100 );
+		final ArrayRandomAccess< UnsignedByteType > ra = img.randomAccess();
+		ra.setPosition( new long[] { 49, 49 } );
+		ra.get().set( 255 );
+
+		System.out.println( "Full strel" );
+		System.out.println( "radius\ttime(ms)" );
+		// Warm up
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), false ), 1 );
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), false ), 1 );
+		for ( int i = 0; i < 40; i++ )
+		{
+			final long start = System.currentTimeMillis();
+			final List< Shape > strels = StructuringElements.diamond( i, img.numDimensions(), false );
+			Dilation.dilate( img, strels, 1 );
+			final long end = System.currentTimeMillis();
+			System.out.println( "" + i + '\t' + ( end - start ) );
+		}
+
+		System.out.println( "Decomposed strel" );
+		System.out.println( "radius\ttime(ms)" );
+		// Warm up
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), true ), 1 );
+		Dilation.dilate( img, StructuringElements.diamond( 1, img.numDimensions(), true ), 1 );
+		for ( int i = 0; i < 40; i++ )
+		{
+			final long start = System.currentTimeMillis();
+			final List< Shape > strels = StructuringElements.diamond( i, img.numDimensions(), true );
+			Dilation.dilate( img, strels, 1 );
+			final long end = System.currentTimeMillis();
+			System.out.println( "" + i + '\t' + ( end - start ) );
+		}
+	}
+}
