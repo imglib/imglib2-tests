0330839f17bf1129841916daccf272042ba0053f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-01-23 17:34:08 +0100

    Interactive test for the Diamond Tips structuring element.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/test/java/net/imglib2/algorithm/morphology/DiamondTipsNeighborhoodTest.java
@@ -0,0 +1,40 @@
+package net.imglib2.algorithm.morphology;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+
+import java.io.File;
+
+import net.imglib2.algorithm.morphology.neighborhoods.DiamondTipsShape;
+import net.imglib2.algorithm.region.localneighborhood.Shape;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+public class DiamondTipsNeighborhoodTest
+{
+	public static < T extends RealType< T > & NativeType< T >> void main( final String[] args )
+	{
+		ImageJ.main( args );
+		final File file = new File( "DrosophilaWing.tif" );
+		final ImagePlus imp = IJ.openImage( file.getAbsolutePath() );
+		final Img< T > img = ImagePlusAdapter.wrap( imp );
+
+		final long start = System.currentTimeMillis();
+
+		final Shape shape = new DiamondTipsShape( 10 );
+		final Img< T > target = Dilation.dilate( img, shape, 1 );
+
+		final long end = System.currentTimeMillis();
+
+		System.out.println( "Processing done in " + ( end - start ) + " ms." );
+
+		ImageJFunctions.show( img );
+		ImageJFunctions.show( target );
+
+	}
+
+}
