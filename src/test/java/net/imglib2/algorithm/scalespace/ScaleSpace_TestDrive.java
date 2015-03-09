fbe2be13dd4c2b15e1174cc286d01a3c7cde3fd6
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-03-09 14:50:02 +0100

    Polish and add 1 use case for scale space test.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/test/java/net/imglib2/algorithm/scalespace/ScaleSpace_TestDrive.java
+++ src/test/java/net/imglib2/algorithm/scalespace/ScaleSpace_TestDrive.java
@@ -6,20 +6,24 @@ import ij.gui.OvalRoi;
 import ij.gui.Overlay;
 import ij.gui.Roi;
 
+import java.awt.Color;
 import java.util.Collection;
 import java.util.Random;
 
 import net.imglib2.RandomAccess;
-import net.imglib2.algorithm.localextrema.RefinedPeak;
+import net.imglib2.algorithm.gauss3.Gauss3;
 import net.imglib2.algorithm.neighborhood.HyperSphereShape;
 import net.imglib2.algorithm.neighborhood.HyperSphereShape.NeighborhoodsAccessible;
 import net.imglib2.algorithm.neighborhood.Neighborhood;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.RealFloatConverter;
+import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
@@ -27,10 +31,84 @@ import net.imglib2.view.Views;
 public class ScaleSpace_TestDrive
 {
 
-	public static void main( final String[] args )
+	public static void main( final String[] args ) throws IncompatibleTypeException
 	{
 		ImageJ.main( args );
+		uniqueBlob();
+		rowsOfBlobs();
+		multipleBlobs();
+	}
 
+	public static void rowsOfBlobs() throws IncompatibleTypeException
+	{
+		final int width = 512;
+		final int height = 512;
+		final ImgFactory< UnsignedByteType > factory = new ArrayImgFactory< UnsignedByteType >();
+		final Img< UnsignedByteType > img = factory.create( new int[] { width, height }, new UnsignedByteType() );
+
+		int radius = 20;
+		for ( int r = 0; r < 4; r++ )
+		{
+			for ( int c = 0; c < 4; c++ )
+			{
+				radius += 2;
+				final HyperSphereShape shape = new HyperSphereShape( radius );
+				final NeighborhoodsAccessible< UnsignedByteType > neighborhoodsAccessible =
+						shape.neighborhoodsRandomAccessible( Views.extendZero( img ) );
+				final RandomAccess< Neighborhood< UnsignedByteType >> randomAccess = neighborhoodsAccessible.randomAccess();
+				randomAccess.setPosition( c * width / 4 + width / 8, 0 );
+				randomAccess.setPosition( r * height / 4 + height / 8, 1 );
+				final int val = 160;
+				for ( final UnsignedByteType pixel : randomAccess.get() )
+				{
+					pixel.set( val );
+				}
+
+			}
+		}
+
+		Gauss3.gauss( 1d, Views.extendMirrorSingle( img ), img );
+		final Random ran = new Random();
+		for ( final UnsignedByteType pixel : img )
+		{
+			pixel.set( ( int ) ( ( pixel.get() + 20 ) * ( 1 + ran.nextDouble() / 4 ) ) );
+		}
+
+		analyze( img );
+	}
+
+	public static void uniqueBlob() throws IncompatibleTypeException
+	{
+
+		final int width = 512;
+		final int height = 512;
+		final int radius = 20;
+		final ImgFactory< UnsignedByteType > factory = new ArrayImgFactory< UnsignedByteType >();
+		final Img< UnsignedByteType > img = factory.create( new int[] { width, height }, new UnsignedByteType() );
+
+		final HyperSphereShape shape = new HyperSphereShape( radius );
+		final NeighborhoodsAccessible< UnsignedByteType > neighborhoodsAccessible =
+				shape.neighborhoodsRandomAccessible( Views.extendZero( img ) );
+		final RandomAccess< Neighborhood< UnsignedByteType >> randomAccess = neighborhoodsAccessible.randomAccess();
+		randomAccess.setPosition( width / 2, 0 );
+		randomAccess.setPosition( height / 2, 1 );
+		final int val = 150;
+		for ( final UnsignedByteType pixel : randomAccess.get() )
+		{
+			pixel.set( val );
+		}
+		Gauss3.gauss( 1d, Views.extendMirrorSingle( img ), img );
+		final Random ran = new Random();
+		for ( final UnsignedByteType pixel : img )
+		{
+			pixel.set( ( int ) ( ( pixel.get() + 20 ) * ( 1 + ran.nextDouble() / 2 ) ) );
+		}
+
+		analyze( img );
+	}
+
+	public static void multipleBlobs() throws IncompatibleTypeException
+	{
 		/*
 		 * Prepare image
 		 */
@@ -52,13 +130,22 @@ public class ScaleSpace_TestDrive
 			final RandomAccess< Neighborhood< UnsignedByteType >> randomAccess = neighborhoodsAccessible.randomAccess();
 			randomAccess.setPosition( ran.nextInt( width ), 0 );
 			randomAccess.setPosition( ran.nextInt( height ), 1 );
-			final int val = ran.nextInt( 256 );
+			final int val = ran.nextInt( 150 );
 			for ( final UnsignedByteType pixel : randomAccess.get() )
 			{
 				pixel.set( val );
 			}
 		}
+		Gauss3.gauss( 1d, Views.extendMirrorSingle( img ), img );
+		for ( final UnsignedByteType pixel : img )
+		{
+			pixel.set( ( int ) ( ( pixel.get() + 20 ) * ( 1 + ran.nextDouble() / 2 ) ) );
+		}
+		analyze( img );
+	}
 
+	public static < T extends RealType< T > & NativeType< T > > void analyze( final Img< T > img )
+	{
 		final ImagePlus imp = ImageJFunctions.wrap( img, "Source" );
 		imp.show();
 
@@ -66,9 +153,9 @@ public class ScaleSpace_TestDrive
 		 * Execute scale space computation.
 		 */
 
-		final double initialSigma = 2;
-		final Converter< UnsignedByteType, FloatType > converter = new RealFloatConverter< UnsignedByteType >();
-		final ScaleSpace< UnsignedByteType > scaleSpace = new ScaleSpace< UnsignedByteType >( img, converter, initialSigma, 0.03d );
+		final double initialSigma = 2.5;
+		final Converter< T, FloatType > converter = new RealFloatConverter< T >();
+		final ScaleSpace< T > scaleSpace = new ScaleSpace< T >( img, converter, initialSigma, 0.03d, 2d );
 		scaleSpace.setNumThreads( 1 );
 
 		System.out.println( "Starting scale space computation." );
@@ -83,34 +170,50 @@ public class ScaleSpace_TestDrive
 		 * Show images
 		 */
 
-		final Img< FloatType > results = scaleSpace.getResult();
-		ImageJFunctions.wrapFloat( results, "Scale space" ).show();
+//		final Img< FloatType > results = scaleSpace.getResult();
+//		ImageJFunctions.wrapFloat( results, "Scale space" ).show();
 
-		final Overlay overlay = new Overlay();
-		imp.setOverlay( overlay );
 
 		/*
 		 * Overlay results on source image
 		 */
 
-		final Collection< RefinedPeak< DifferenceOfGaussianPeak >> peaks = scaleSpace.getPeaks();
-		for ( final RefinedPeak< DifferenceOfGaussianPeak > peak : peaks )
+		final Overlay overlay = new Overlay();
+		imp.setOverlay( overlay );
+		overlay.setStrokeColor( Color.GREEN );
+
+		final Collection< Blob > brightBlobs = scaleSpace.getBrightBlobs();
+		System.out.println( "Found " + brightBlobs.size() + " bright blobs." );
+		for ( final Blob blob : brightBlobs )
 		{
+			System.out.println( " - " + blob );
 
-			if ( !peak.getOriginalPeak().isMax() )
-			{
-				continue; // only maxima
-			}
-
-			final double diameter = 2 * peak.getDoublePosition( 2 ) * Math.sqrt( img.numDimensions() );
+			final double diameter = 2 * blob.getRadius();
 			final Roi roi = new OvalRoi(
-					0.5 + peak.getDoublePosition( 0 ) - diameter / 2,
-					0.5 + peak.getDoublePosition( 1 ) - diameter / 2,
+					0.5 + blob.getDoublePosition( 0 ) - diameter / 2,
+					0.5 + blob.getDoublePosition( 1 ) - diameter / 2,
 					diameter, diameter );
-			overlay.add( roi );
-		}
 
-		imp.show();
+			overlay.add( roi );
+			imp.updateAndDraw();
+		}
+		overlay.setStrokeColor( Color.RED );
+
+		final Collection< Blob > darkBlobs = scaleSpace.getDarkBlobs();
+		System.out.println( "Found " + darkBlobs.size() + " dark blobs." );
+		for ( final Blob blob : darkBlobs )
+		{
+			System.out.println( " - " + blob );
+
+			final double diameter = 2 * blob.getRadius();
+			final Roi roi = new OvalRoi(
+					0.5 + blob.getDoublePosition( 0 ) - diameter / 2,
+					0.5 + blob.getDoublePosition( 1 ) - diameter / 2,
+					diameter, diameter );
+
+			overlay.add( roi );
+			imp.updateAndDraw();
+		}
 	}
 
 }
