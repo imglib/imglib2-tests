ed932291c2b64c68f5cd5b9e050e7b3dd3840911
Curtis Rueden <ctrueden@wisc.edu>
2016-05-03 15:07:40 -0500

    chart-gen.py: switch from Flot to pChart
    We want to embed these charts into the ImageJ wiki now.
    
    See: https://www.mediawiki.org/wiki/Extension:Pchart4mw

--- src/test/scripts/chart-gen.py
+++ src/test/scripts/chart-gen.py
@@ -25,7 +25,7 @@
 # #L%
 ###
 
-# Transforms CSV data into HTML that uses Flot Javascript charts.
+# Transforms CSV data into HTML that uses pChart4mw charts.
 
 # Charts produced:
 #   - Iteration x Time for minimum resolution image, by method
@@ -37,58 +37,63 @@
 
 import math, os, string
 
-# NB: Presumably there is a slick way to avoid having two nearly identical
-# methods (generateIterationChart and generateResolutionChart) iterating over
-# different list indices, but my Python knowledge is weak.
+# NB: Presumably there is a slick way to avoid having two very similar
+# methods (generateIterationChart and generateResolutionChart) iterating
+# over different list indices, but my Python knowledge is weak.
 
 # Iteration x Time at a given resolution, by method
-def generateIterationChart(name, data, methods, resolution_index):
-    print '    // Iteration x Time at resolution #' + str(resolution_index) + ':'
-    resolution_count = len(data)
-    iteration_count = len(data[0])
-    method_count = len(methods)
-    print '    var ' + name + ' = {'
-    for m in range(0, method_count):
-        print '        "method' + str(m + 1) + '": {'
-        print '            label: "' + methods[m] + '",'
-        print '            data: [',
-        for i in range(0, iteration_count):
-            if i < iteration_count - 1:
-                suffix = '],'
-            else:
-                suffix = ']'
-            print '[' + str(i) + ', ' + str(data[resolution_index][i][m]) + suffix,
-        print '],'
-        checked = methods[m] == 'ImageJ' \
-            or methods[m] == 'Imglib (Array)' \
-            or methods[m] == 'Imglib (Planar)' \
-            or methods[m] == 'Raw';
-        print '            checked: ' + string.lower(str(checked))
-        print '        },'
-    print '    };'
-
-# Resolution x Time at a given iteration, by method
-def generateResolutionChart(name, data, methods, iteration_index):
-    print '    // Resolution x Time at iteration #' + str(iteration_index) + ':'
+def generateIterationChart(name, data, methods, resolution_index, keywords):
     resolutions = range(1, 26, 3)
     resolution_count = len(data)
     iteration_count = len(data[0])
     method_count = len(methods)
-    print '    var ' + name + ' = {'
+    title = 'Iteration x Time (ms) at ' + str(resolutions[resolution_index]) + ' Mpx'
+    div = "legend" not in keywords
+    if div:
+        print '<div style="float: left">'
+    print '<pLines ymin=0 title="' + title + '" ' + keywords + '>'
+    # Header
     for m in range(0, method_count):
-        print '        "method' + str(m + 1) + '": {'
-        print '            label: "' + methods[m] + '",'
-        print '            data: [',
-        for r in range(0, resolution_count):
-            if r < resolution_count - 1:
-                suffix = '],'
-            else:
-                suffix = ']'
-            print '[' + str(resolutions[r]) + ', ' + \
-                str(data[r][iteration_index][m]) + suffix,
-        print ']'
-        print '        },'
-    print '    };'
+        suffix = ',' if m < method_count - 1 else ''
+        print methods[m] + suffix,
+    print
+    # Data
+    for i in range(0, iteration_count):
+        for m in range(0, method_count):
+            suffix = ',' if m < method_count - 1 else ''
+            print str(data[resolution_index][i][m]) + suffix,
+        print
+    print '</pLines>'
+    if div:
+        print '</div>'
+
+# Resolution x Time at a given iteration, by method
+def generateResolutionChart(name, data, methods, iteration_index, keywords):
+    resolutions = range(1, 26, 3)
+    resolution_count = len(data)
+    iteration_count = len(data[0])
+    method_count = len(methods)
+    title = 'Resolution x Time (ms) at iteration #' + str(iteration_index + 1)
+    div = "legend" not in keywords
+    if div:
+        print '<div style="float: left">'
+    print '<pLines ymin=0 title="' + title + '" ' + keywords + '>'
+    # Header
+    print ',',
+    for m in range(0, method_count):
+        suffix = ',' if m < method_count - 1 else ''
+        print methods[m] + suffix,
+    print
+    # Data
+    for r in range(0, resolution_count):
+        print str(resolutions[r]) + ' Mpx,',
+        for m in range(0, method_count):
+            suffix = ',' if m < method_count - 1 else ''
+            print str(data[r][iteration_index][m]) + suffix,
+        print
+    print '</pLines>'
+    if div:
+        print '</div>'
 
 # reads data from CSV files into 3D array dimensioned:
 # [resolution_count][iteration_count][method_count]
@@ -129,16 +134,27 @@ def process(prefix):
     iteration_count = len(data[0])
     method_count = len(methods)
 
+    w = 300
+    h = 250
+    l_size = 'size=' + str(w) + 'x' + str(h)
+    r_size = 'size=' + str(w + 135) + 'x' + str(h)
+
     # Iteration x Time for minimum resolution image, by method
-    generateIterationChart('res_' + prefix + '_min', data, methods, 0)
+    generateIterationChart('res_' + prefix + '_min', \
+        data, methods, 0, l_size + ' plots')
     # Iteration x Time for maximum resolution image, by method
     generateIterationChart('res_' + prefix + '_max', \
-        data, methods, resolution_count - 1)
+        data, methods, resolution_count - 1, r_size + ' plots legend')
+    print '{{Clear}}'
     # Resolution x Time at first iteration, by method
-    generateResolutionChart('iter_' + prefix + '_first', data, methods, 0)
+    generateResolutionChart('iter_' + prefix + '_first', \
+        data, methods, 0, l_size + ' angle=90 cubic plots')
     # Resolution x Time at last iteration, by method
     generateResolutionChart('iter_' + prefix + '_last', \
-        data, methods, iteration_count - 1)
+        data, methods, iteration_count - 1, r_size + ' angle=90 cubic plots legend')
+    print '{{Clear}}'
 
+print '== Cheap operation results =='
 process('cheap')
+print '== Expensive operation results =='
 process('expensive')
